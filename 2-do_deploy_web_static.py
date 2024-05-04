1c437d294cad3effedf1a87a1ef6b8f4d739c317
5alidev <khalidbenhamou.dev@outlook.com>
2024-05-03 18:10:13 -0700

    update fabric script for deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ archive to the web servers using the function do_deploy
 """
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['54.162.97.17', '34.224.83.106']
 
 
 def do_deploy(archive_path):
